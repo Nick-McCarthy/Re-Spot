d848c8dd49543d876a560f8d633dc9ac95747e17
HoldyClaus <holdenclaus0212@gmail.com>
2023-03-10 12:57:51 -0600

    changes to header

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -9,13 +9,13 @@ const Header = () => {
     Auth.logout();
   };
   return (
-    <header className="bg-primary text-light mb-4 py-3 flex-row align-center">
+    <header className="bg-black mb-4 py-3 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
         <div>
-          <Link className="text-light" to="/">
-            <h1 className="m-0">Review Spotify Playlist</h1>
+          <Link className="" to="/" style={{ color: 'green' }}>
+            <h1 className="m-0 text-green"> Spotify Playlist Review</h1>
           </Link>
-          <p className="m-0">Discuss your peers playlist.</p>
+          <p className="m-0 text-white">Discuss your peers playlist.</p>
         </div>
         <div>
           {Auth.loggedIn() ? (
@@ -23,16 +23,16 @@ const Header = () => {
               <Link className="btn btn-lg btn-info m-2" to="/me">
                 {Auth.getProfile().data.username}'s profile
               </Link>
-              <button className="btn btn-lg btn-light m-2" onClick={logout}>
+              <button className="btn btn-lg btn-light m-2" style={{ color: 'white' }} onClick={logout}>
                 Logout
               </button>
             </>
           ) : (
             <>
-              <Link className="btn btn-lg btn-info m-2" to="/login">
+              <Link className="btn btn-lg btn-info m-2" to="/login" >
                 Login
               </Link>
-              <Link className="btn btn-lg btn-light m-2" to="/signup">
+              <Link className="btn btn-lg btn-light m-2" to="/signup" style={{ color: 'black' }}>
                 Signup
               </Link>
             </>
