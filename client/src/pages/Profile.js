ddc65e9dff397a0738f180469d949605afb80167
ZechB7 <117792233+ZechB7@users.noreply.github.com>
2023-03-09 20:47:40 -0600

    dont push the lock3

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -39,7 +39,7 @@ const Profile = () => {
     <div>
       <div className="flex-row justify-center mb-3">
         <h2 className="col-12 col-md-10 bg-dark text-light p-3 mb-5">
-          Viewing {userParam ? `${user.username}'s` : 'your'} profile.
+          Viewing {userParam ? `${user.username}'s` : 'your'} profile. addding just so i can push
         </h2>
 
         <mapOverthis>
